46f95f571b5aa64a2ff58a3c5cb14ec0b3f9f8d4
Taisuke Hashimoto <hashimoto.libra+development@gmail.com>
2015-09-04 23:31:28 +0900

    元リセットに関するゴミの処理

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,20 +6,12 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
 
     private Vector3 offset;
-    private Vector3 initialOffset;
-
-    private Quaternion initialRot;
-    private Vector3 initialPos;
 
 	// Use this for initialization
 	void Start () {
-        initialRot = transform.rotation;
-        initialPos = transform.position;
-
         if (player == null) player = GameObject.Find("Player");
         //offset = transform.position-player.transform.position;
         offset = new Vector3(0f, 2.5f, -3f);
-        initialOffset = offset;
 	}
 
     //void LateUpdate () {
@@ -34,11 +26,4 @@ public class CameraController : MonoBehaviour {
             transform.RotateAround(player.transform.position, Vector3.up, -5);
         offset = transform.position - player.transform.position;
     }
-
-    //public void Reset()
-    //{
-    //    transform.rotation = initialRot;
-    //    transform.position = initialPos;
-    //    offset = initialOffset;
-    //}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,9 @@ public class PlayerController : MonoBehaviour {
     public float jumpingForce = 300f;
 
     private int layerMask;
-    private Quaternion initialRot;
-    private Vector3 initialPos;
 
     void Start()
     {
-        initialRot = transform.rotation;
-        initialPos = transform.position;
-
         if (mainCamera == null) mainCamera = GameObject.Find("MainCamera");
         layerMask = (1 << LayerMask.NameToLayer("Field")) + (1 << LayerMask.NameToLayer("RealObject"));
     }
@@ -36,12 +31,4 @@ public class PlayerController : MonoBehaviour {
         if (Physics.Raycast(transform.position, -Vector3.up, this.GetComponent<SphereCollider>().radius, layerMask)) return false;
         else return true;
     }
-
-    //public void Reset()
-    //{
-    //    transform.rotation = initialRot;
-    //    transform.position = initialPos;
-    //    GetComponent<Rigidbody>().velocity = Vector3.zero;
-    //    GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-    //}
 }

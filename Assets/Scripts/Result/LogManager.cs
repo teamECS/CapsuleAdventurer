be4756850e837d1e809e58552ab2029081291151
Taisuke Hashimoto <hashimoto.libra+development@gmail.com>
2015-11-13 04:55:21 +0900

    LogManagerの実装(途中)

---
+++ Assets/Scripts/Result/LogManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+using System.IO;
+
+public class LogManager : MonoBehaviour
+{
+    private string filePath;
+    private IList logList;
+
+	// Use this for initialization
+	void Start () {
+
+        filePath = Application.dataPath + "/data/log.json";
+        if (!File.Exists(filePath)){
+            Debug.Log(filePath);
+            Debug.Log("logファイルがありません！");
+            return;
+        }
+
+        string jsonStr = File.ReadAllText(filePath);
+        logList = Json.Deserialize(jsonStr) as IList;
+	}
+
+    void AddLog(string name, int score)
+    {
+        var dic = new Dictionary<string, object>();
+        dic.Add("name", name);
+        dic.Add("score", score);
+
+        logList.Add(dic);
+        string jsonStr = Json.Serialize(logList);
+        File.WriteAllText(filePath, jsonStr);
+    }
+
+    void PrintLog() {
+        foreach (IDictionary player in logList)
+        {
+            Debug.Log(player["name"]);
+            Debug.Log(player["score"]);
+        }
+    }
+}

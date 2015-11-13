f0869bbe6da09820139e71033dd27a59506d99d8
Taisuke Hashimoto <hashimoto.libra+development@gmail.com>
2015-11-13 16:22:45 +0900

    scoreTextを外部から更新する関数を作成

--- Assets/Scripts/Result/ScoreTextGenerater.cs
+++ Assets/Scripts/Result/ScoreTextGenerater.cs
@@ -8,12 +8,13 @@ public class ScoreTextGenerater : MonoBehaviour {
     public Text scoreText;
 
 	// Use this for initialization
-	void Start () {
-        if(scoreText==null) scoreText = GetComponent<Text>();
+    void Start()
+    {
+        if (scoreText == null) scoreText = GetComponent<Text>();
         scoreText.text = "あなたのスコア：" + gameManager.GetScore();
-	}
+    }
 
-    void Update() {
+    public void UpdateText() {
         if (gameManager.GetPlayerName() != "") scoreText.text = "「" + gameManager.GetPlayerName() + "」さんのスコア：" + gameManager.GetScore();
         else scoreText.text = "あなたのスコア：" + gameManager.GetScore();
     }

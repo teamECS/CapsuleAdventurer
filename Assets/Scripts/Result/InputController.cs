c3de523326f69510d87b2f89f7196ef27a45128a
Taisuke Hashimoto <hashimoto.libra+development@gmail.com>
2015-11-13 16:21:38 +0900

    クリック時の動作をまとめた

--- Assets/Scripts/Result/InputController.cs
+++ Assets/Scripts/Result/InputController.cs
@@ -6,12 +6,27 @@ public class InputController : MonoBehaviour {
 
     public GameManager gameManager;
     public InputField inputField;
-	
-    public void SetPlayerName()
+
+    public LogManager logManager;
+
+    public ScoreTextGenerater scoreTextGenerater1;
+    public ScoreTextGenerater scoreTextGenerater2;
+
+    public ResultController resultController;
+    public GameObject buttonObj;
+
+    public void OnClick()
     {
         if (inputField.text != "")
         {
             gameManager.SetPlayerName(inputField.text);
+            logManager.AddLog();
+            scoreTextGenerater1.UpdateText();
+            scoreTextGenerater2.UpdateText();
+
+            resultController.HideMenus();
+            resultController.DisplayResultMenu();
+            resultController.SetInteractive(buttonObj, false);
         }
     }
 }
